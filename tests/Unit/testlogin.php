71122a46887ab9726d1877edf6f195bc592cbff4
salaheddine <salaheddieeliysaouy@gmail.com>
2026-04-17 21:56:17 +0100

    test_login_page

--- tests/Unit/testlogin.php
+++ tests/Unit/testlogin.php
@@ -1,21 +1,8 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class testlogin extends TestCase
+class TestLogin extends TestCase
 {
-    private $cookieFile;
-
-    protected function setUp(): void
-    {
-        // fichier pour garder la session
-        $this->cookieFile = tempnam(sys_get_temp_dir(), 'cookie');
-    }
-
-    protected function tearDown(): void
-    {
-        unlink($this->cookieFile);
-    }
-
     private function request($url, $method = 'GET', $data = [])
     {
         $ch = curl_init();
@@ -29,31 +16,22 @@ class testlogin extends TestCase
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
 
-        //  gestion session (très important)
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookieFile);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookieFile);
-
         $response = curl_exec($ch);
-
-        $headerSize = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 
         return [
             'code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
-            'headers' => substr($response, 0, $headerSize),
-            'body' => substr($response, $headerSize),
+            'body' => $response,
         ];
     }
 
-    // ✅ Test login SUCCESS
     public function testLoginSuccess()
     {
         $response = $this->request('/index/login_page.php', 'POST', [
             'email'    => 'reclamant@email.com',
-            'password' => '$2y$10$iIcAHnS8qJxXVEXesneiUOpIVpC72NBKx7LGfb/ee355zfmCOjH1m',
+            'password' => 'reclamant',
         ]);
 
         $this->assertEquals(302, $response['code']);
-        $this->assertStringContainsString('espace_reclamation.php', $response['headers']);
+        $this->assertStringContainsString('espace_reclamation.php', $response['body']);
     }
-
 }
